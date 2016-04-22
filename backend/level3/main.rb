d8fc0e5998b39a55522d0e4b54e5bb7a0b55152b
edouard winia <edouardwinia@gmail.com>
2016-04-22 23:47:27 +0200

    refacto ok

--- backend/level3/main.rb
+++ backend/level3/main.rb
@@ -9,16 +9,13 @@ require_relative 'commission'
     results = Array.new
     raw_input["rentals"].each do |rental_data|
       rental = Rental.new(rental_data)
-      car_array = raw_input["cars"].select { |car| car["id"] == rental.car_id }
-      car = Car.new(car_array)
+      car = Car.new(raw_input["cars"].select { |car| car["id"] == rental.car_id })
       rental.price = rental.compute_rental_price(car.price_per_km, car.price_per_day)
       commission = Commission.new.computation_of_commission_amount_and_fees(rental.price, rental.length)
       results << prepare_intermediary_result_hash_of_data(rental.id, rental.price, commission)
     end
-    final_result = Hash.new
-    final_result["rentals"] = results
-    write_to_json_file(final_result)
-    puts JSON.pretty_generate(final_result)
+    write_to_json_file(prepare_hash_of_date_to_be_transfered_to_json(results))
+    # puts JSON.pretty_generate(prepare_hash_of_date_to_be_transfered_to_json(results))
   end
 
   private
@@ -31,12 +28,16 @@ require_relative 'commission'
     return res
   end
 
+  def prepare_hash_of_date_to_be_transfered_to_json(results)
+    final_result = Hash.new
+    final_result["rentals"] = results
+  end
+
   def write_to_json_file(final_result)
     File.open('output2.json', 'w') do |file|
       # before submiting file to Drivy, replace output2.json with output.json
       file.write(JSON.generate(final_result))
     end
-    puts JSON.pretty_generate(final_result)
   end
 
   produce_output_data_from_input_data
