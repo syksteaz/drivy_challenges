1c4d23b527bf8d5fd320ff25994ad668253c3d20
edouard winia <edouardwinia@gmail.com>
2016-05-04 11:32:22 +0200

    ok refacto rental done

--- backend/level2/rental.rb
+++ backend/level2/rental.rb
@@ -1,6 +1,11 @@
 class Rental
   attr_accessor :id, :car_id, :distance, :start_date, :end_date, :nb_of_days
 
+  DISCOUNT_BY_10_MIN_THRESHOLD = 1
+  DISCOUNT_BY_30_MIN_THRESHOLD = 4
+  DISCOUNT_BY_50_MIN_THRESHOLD = 10
+  MAX = 1000
+
   def initialize(rental = {})
     @id = rental['id']
     @car_id = rental['car_id']
@@ -10,55 +15,47 @@ class Rental
     @nb_of_days = end_date - start_date + 1
   end
 
-  # def computation_of_distance_component(car)
-  #   self.price_distance_component = distance * car.price_per_km
-  # end
-
-  def computation_of_price_time_component(car)
-    self.define_price_variables(car)
-    self.compute_nb_of_days_at_each_price
-    self.discount_component_of_price
-
-    @full_price_component.to_i +
-      @price_discount_by_10_component.to_i +
-      @price_discount_by_30_component.to_i +
-      @price_discount_by_50_component.to_i
-  end
-
-  def define_price_variables(car)
-    @price_per_day = car.price_per_day # pas indispensable de remettre ça dans une variable
-    @price_per_day_discounted_by_10 = @price_per_day * 0.9 # pas indispensable
-    @price_per_day_discounted_by_30 = @price_per_day * 0.7
-    @price_per_day_discounted_by_50 = @price_per_day * 0.5
-    @discount_by_10_min_threshold = 1 # ici ça pourrait être des constantes plutôt que des variables d'instance
-    @discount_by_10_max_threshold = 4
-    @discount_by_30_min_threshold = 4 # ne pas répéter deux fois les seuils identiques
-    @discount_by_30_max_threshold = 10
-    @discount_by_50_min_threshold = 10 #
-  end
-
-  def compute_nb_of_days_at_each_price # ici on pourrait checker en entrée si nb_of_days < min threshold et mettre 0
-    # pour réllement calculer le nombre de jours
-    # ici on pourrait créer un array d'array avec le résultat des calcul plutot que des pseudos variables d'instance
-    @days_at_full_price = self.nb_of_days >= 1 ? 1 : self.nb_of_days
-    @days_at_10_discount = self.nb_of_days > @discount_by_10_max_threshold ? (@discount_by_10_max_threshold -
-      @discount_by_10_min_threshold) : (self.nb_of_days - @discount_by_10_min_threshold)
-    @days_at_30_discount = self.nb_of_days > @discount_by_30_max_threshold ? (@discount_by_30_max_threshold -
-      @discount_by_30_min_threshold) : (self.nb_of_days - @discount_by_30_min_threshold)
-    @days_at_50_discount = self.nb_of_days > @discount_by_50_min_threshold ? (self.nb_of_days -
-      @discount_by_50_min_threshold) : 0
-  end
-
-  def discount_component_of_price
-    @full_price_component = (@days_at_full_price > 0 ? @days_at_full_price : 0) * @price_per_day
-    @price_discount_by_10_component = (@days_at_10_discount > 0 ? @days_at_10_discount : 0) * @price_per_day_discounted_by_10
-    @price_discount_by_30_component = (@days_at_30_discount > 0 ? @days_at_30_discount : 0) * @price_per_day_discounted_by_30
-    @price_discount_by_50_component = (@days_at_50_discount > 0 ? @days_at_50_discount : 0) * @price_per_day_discounted_by_50
-  end
-
-  def computation_of_total_price(car) # attention ici c'est faux (2 fois la même chose, et distance component n'est plus utilisé)
-    (self.computation_of_price_time_component(car) +
+  def computation_of_total_price(car)
+    (self.computation_of_price_distance_component(car) +
     self.computation_of_price_time_component(car)).to_i
   end
 
+  def computation_of_price_distance_component(car)
+    @distance * car.price_per_km
+  end
+
+  def computation_of_price_time_component(car)
+    self.compute_discounted_price(car, compute_nb_of_days_at_each_price)
+  end
+
+  def compute_discounted_price(car, count_of_nb_of_days_at_each_price)
+    count_of_nb_of_days_at_each_price[0][:full_price] * car.price_per_day +
+    count_of_nb_of_days_at_each_price[0][:discount_10] * car.price_per_day * 0.9 +
+    count_of_nb_of_days_at_each_price[0][:discount_30] * car.price_per_day * 0.7 +
+    count_of_nb_of_days_at_each_price[0][:discount_50] * car.price_per_day * 0.5
+  end
+
+  def compute_nb_of_days_at_each_price
+    count_of_nb_of_days_at_each_price = []
+    nb_of_days_at_each_price_detail = {}
+    if self.nb_of_days >= DISCOUNT_BY_10_MIN_THRESHOLD
+      nb_of_days_at_each_price_detail[:full_price] = 1
+    else
+      nb_of_days_at_each_price_detail[:full_price] = self.nb_of_days
+    end
+    self.nb_of_days_at_price_loop(DISCOUNT_BY_10_MIN_THRESHOLD, DISCOUNT_BY_30_MIN_THRESHOLD, nb_of_days_at_each_price_detail, :discount_10 )
+    self.nb_of_days_at_price_loop(DISCOUNT_BY_30_MIN_THRESHOLD, DISCOUNT_BY_50_MIN_THRESHOLD, nb_of_days_at_each_price_detail, :discount_30 )
+    self.nb_of_days_at_price_loop(DISCOUNT_BY_50_MIN_THRESHOLD, MAX, nb_of_days_at_each_price_detail, :discount_50 )
+    count_of_nb_of_days_at_each_price << nb_of_days_at_each_price_detail
+  end
+
+  def nb_of_days_at_price_loop(min_threshold, max_threshold, hash_name, symbol_for_hash)
+    if self.nb_of_days <= min_threshold
+      hash_name[symbol_for_hash] = 0
+    elsif self.nb_of_days > max_threshold
+      hash_name[symbol_for_hash] = max_threshold - min_threshold
+    else
+      hash_name[symbol_for_hash] = self.nb_of_days - min_threshold
+    end
+  end
 end
